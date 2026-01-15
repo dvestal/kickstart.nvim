2546ec22b87d2254a7d00523df97c29291a12f01
David Vestal <vestal.d@gmail.com>
2026-01-14 20:59:28 -0600

    adding gemini-cli

--- init.lua
+++ init.lua
@@ -166,6 +166,11 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -984,7 +989,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'marcinjahn/gemini-cli.nvim',
+    cmd = 'Gemini',
+    keys = {
+      { '<leader>a/', '<cmd>Gemini toggle<cr>', desc = 'Toggle Gemini CLI' },
+      { '<leader>aa', '<cmd>Gemini ask<cr>', desc = 'Ask Gemini', mode = { 'n', 'v' } },
+      { '<leader>af', '<cmd>Gemini add_file<cr>', desc = 'Add File' },
+    },
+    dependencies = {
+      'folke/snacks.nvim',
+    },
+    config = true,
+  },
+}
